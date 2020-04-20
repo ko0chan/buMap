249843c98cd16e9a193ee9b33e6625a533e43d8f
YoungChan <kychan1320@gmail.com>
2020-04-20 17:27:50 +0900

    kotli fragment naver map 객체 생성

--- app/src/main/java/com/example/bumap/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/bumap/ui/home/HomeFragment.kt
@@ -4,13 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.annotation.UiThread
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.bumap.R
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.*
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -23,6 +24,22 @@ class HomeFragment : Fragment() {
                 ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
 
+        val fm = childFragmentManager
+        val mapFragment = fm.findFragmentById(R.id.map) as MapFragment?
+            ?: MapFragment.newInstance().also {
+                fm.beginTransaction().add(R.id.map, it).commit()
+            }
+//        val textView: TextView = root.findViewById(R.id.text_home)
+//        homeViewModel.text.observe(viewLifecycleOwner, Observer {
+//            textView.text = it
+//        })
+
+        mapFragment.getMapAsync(this)
         return root
+    }
+
+    @UiThread
+    override fun onMapReady(naverMap: NaverMap) {
+        // ...
     }
 }
